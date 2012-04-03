643879531f62bfb053435e3ec9c28c0682230d2a
Zachary Anker <zach.anker@gmail.com>
2012-04-03 15:33:08 -0700

    Added HTTPError

--- lib/google-contacts/exceptions.rb
+++ lib/google-contacts/exceptions.rb
@@ -1,3 +1,13 @@
 module GContacts
   class MissingToken < RuntimeError; end
+
+  class HTTPError < StandardError
+    attr_reader :reply_code
+
+    def initialize(msg, reply_code=nil)
+      super(msg)
+      @reply_code = reply_code
+    end
+  end
+
 end
