fcacb758ff7325e1827632b9b291ee83069836ab
Zachary Anker <zach.anker@gmail.com>
2012-04-03 20:08:43 -0700

    Added a container class for all the entries

--- lib/google-contacts/list.rb
+++ lib/google-contacts/list.rb
@@ -1,7 +1,31 @@
 module GContacts
   class List
-    attr_reader :id, :updated, :category, :title
+    include Enumerable
+    extend Forwardable
 
+    attr_reader :id, :updated, :title, :author, :per_page, :start_index, :total_results, :next_uri
 
+    def_delegators :@entries, :each, :[]
+
+    def initialize(data)
+      data = data["feed"]
+
+      type = data["category"]["@term"].split("#", 2).last
+      if type == "contact"
+        @entries = data["entry"].map {|entry| Element.new(entry)}
+      else
+        raise InvalidKind, "Google element of kind #{type} is not supported"
+      end
+
+      @id, @updated, @title, @author = data["id"], data["updated"], data["title"], data["author"]
+      @per_page, @start_index, @total_results = data["openSearch:itemsPerPage"].to_i, data["openSearch:startIndex"].to_i, data["openSearch:totalResults"].to_i
+
+      data["link"].each do |link|
+        if link["@rel"] == "next"
+          @next_uri = URI(link["@href"]).request_uri
+          break
+        end
+      end
+    end
   end
 end
